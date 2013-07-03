49bdf15e662a2b256febe22b0cb05f845e92db7c
Joe <mooman219@gmail.com>
2013-07-03 02:14:43 -0400

    Finish up minerals...
    Finish the last method needed to be updated.
    Now to fix up regions then test!

--- src/main/java/com/gmail/mooman219/module/mineral/command/ListMinerals.java
+++ src/main/java/com/gmail/mooman219/module/mineral/command/ListMinerals.java
@@ -3,13 +3,14 @@ package com.gmail.mooman219.module.mineral.command;
 import org.bukkit.Effect;
 import org.bukkit.entity.Player;
 
-import com.gmail.mooman219.bull.CDChunk;
 import com.gmail.mooman219.bull.CDPlayer;
 import com.gmail.mooman219.frame.WorldHelper;
 import com.gmail.mooman219.frame.command.CCommand;
 import com.gmail.mooman219.frame.rank.Rank;
+import com.gmail.mooman219.frame.serialize.json.BasicVectorInteger;
 import com.gmail.mooman219.module.mineral.CCMineral;
 import com.gmail.mooman219.module.mineral.store.BasicMineral;
+import com.gmail.mooman219.module.mineral.store.StoreMineral;
 
 public class ListMinerals extends CCommand {
     public ListMinerals() {
@@ -18,12 +19,12 @@ public class ListMinerals extends CCommand {
 
     @Override
     public void processPlayer(Player sender, CDPlayer playerData, String[] args) {
-        CDChunk chunk = CDChunk.get(sender);
-        CCMineral.FRM.LIST_TITLE.send(sender, chunk.getMinerals().size());
+        CCMineral.FRM.LIST_TITLE.send(sender, StoreMineral.getMinerals().size());
         int i = 0;
-        for(BasicMineral mineral : chunk.getMinerals()) {
-            CCMineral.FRM.LIST.send(sender, i, mineral.getBasicLocation(), mineral.y, mineral.z, mineral.respawnDelay);
-            WorldHelper.playEffect(mineral.getLocation(chunk), Effect.MOBSPAWNER_FLAMES);
+        for(BasicMineral mineral : StoreMineral.getMinerals().values()) {
+            BasicVectorInteger bl = mineral.getBasicLocation().getVector();
+            CCMineral.FRM.LIST.send(sender, i, bl.getX(), bl.getY(), bl.getZ(), mineral.getRespawnDelay() / 1000);
+            WorldHelper.playEffect(mineral.getLocation(), Effect.MOBSPAWNER_FLAMES);
             i++;
         }
     }
