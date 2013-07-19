e2f997557619c7d51b224aed093bff8dd0f7bbe6
Joe <mooman219@gmail.com>
2013-07-19 06:56:39 -0400

    OPTIMIZE reading lores

--- src/main/java/com/gmail/mooman219/frame/item/AspectItem.java
+++ src/main/java/com/gmail/mooman219/frame/item/AspectItem.java
@@ -81,6 +81,7 @@ public class AspectItem {
             while(iterator.hasNext()) {
                 if(iterator.next().read(line)) {
                     iterator.remove();
+                    break;
                 }
             }
         }
