4f6822c457984b8101dbbedbeb16f93b88c0c4cc
Joe <mooman219@gmail.com>
2013-07-30 23:26:19 -0400

    Don't really need the placeholders

--- src/main/java/com/gmail/mooman219/module/discipline/Codeplaceholder.java
+++
@@ -1,5 +0,0 @@
-package com.gmail.mooman219.module.discipline;
-
-public class Codeplaceholder {
-
-}
--- src/main/java/com/gmail/mooman219/module/mount/Codeplaceholder.java
+++
@@ -1,5 +0,0 @@
-package com.gmail.mooman219.module.mount;
-
-public class Codeplaceholder {
-
-}
