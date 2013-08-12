bdcd693574522d78759c94f3e4d49578c19518fd
Joe <mooman219@gmail.com>
2013-08-12 19:27:46 -0400

    Remove useless damage code

--- src/main/java/com/gmail/mooman219/module/damage/event/PlayerDamageByLivingEvent.java
+++ src/main/java/com/gmail/mooman219/module/damage/event/PlayerDamageByLivingEvent.java
@@ -1,27 +1,19 @@
 package com.gmail.mooman219.module.damage.event;
 
-import org.bukkit.event.Cancellable;
-import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
 import com.gmail.mooman219.bull.CDLiving;
 import com.gmail.mooman219.bull.CDPlayer;
 import com.gmail.mooman219.module.damage.api.DamageType;
 
-public class PlayerDamageByLivingEvent extends Event implements Cancellable {
+public class PlayerDamageByLivingEvent extends PlayerDamageEvent {
     private static final HandlerList handlers = new HandlerList();
 
-    private boolean cancelled = false;
-    private final CDPlayer target;
     private final CDLiving damager;
-    private final DamageType damageType;
-    private double damage;
 
     public PlayerDamageByLivingEvent(CDPlayer target, CDLiving damager, DamageType damageType, double damage) {
-        this.target = target;
+        super(target, damageType, damage);
         this.damager = damager;
-        this.damageType = damageType;
-        this.damage = damage;
     }
 
     @Override
@@ -33,33 +25,7 @@ public class PlayerDamageByLivingEvent extends Event implements Cancellable {
         return handlers;
     }
 
-    public CDPlayer getTarget() {
-        return target;
-    }
-
     public CDLiving getDamager() {
         return damager;
-    }
-
-    public DamageType getDamageType() {
-        return damageType;
-    }
-
-    public double getDamage() {
-        return damage;
-    }
-
-    @Override
-    public boolean isCancelled() {
-        return cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    public void setDamage(double damage) {
-        this.damage = damage;
     }
 }
--- src/main/java/com/gmail/mooman219/module/damage/event/PlayerDamageByPlayerEvent.java
+++ src/main/java/com/gmail/mooman219/module/damage/event/PlayerDamageByPlayerEvent.java
@@ -1,26 +1,18 @@
 package com.gmail.mooman219.module.damage.event;
 
-import org.bukkit.event.Cancellable;
-import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
 import com.gmail.mooman219.bull.CDPlayer;
 import com.gmail.mooman219.module.damage.api.DamageType;
 
-public class PlayerDamageByPlayerEvent extends Event implements Cancellable {
+public class PlayerDamageByPlayerEvent extends PlayerDamageEvent {
     private static final HandlerList handlers = new HandlerList();
 
-    private boolean cancelled = false;
-    private final CDPlayer target;
     private final CDPlayer damager;
-    private final DamageType damageType;
-    private double damage;
 
     public PlayerDamageByPlayerEvent(CDPlayer target, CDPlayer damager, DamageType damageType, double damage) {
-        this.target = target;
+        super(target, damageType, damage);
         this.damager = damager;
-        this.damageType = damageType;
-        this.damage = damage;
     }
 
     @Override
@@ -32,33 +24,7 @@ public class PlayerDamageByPlayerEvent extends Event implements Cancellable {
         return handlers;
     }
 
-    public CDPlayer getTarget() {
-        return target;
-    }
-
     public CDPlayer getDamager() {
         return damager;
-    }
-
-    public DamageType getDamageType() {
-        return damageType;
-    }
-
-    public double getDamage() {
-        return damage;
-    }
-
-    @Override
-    public boolean isCancelled() {
-        return cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    public void setDamage(double damage) {
-        this.damage = damage;
     }
 }
